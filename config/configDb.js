8ab6e9dae15264d60fced877cf427209dc453915
DESKTOP-VCU0LVE\edgar <byteknown@gmail.com>
2022-01-03 12:38:36 +0100

    same

---
+++ config/configDb.js
@@ -0,0 +1,17 @@
+const config = {
+
+    host: "sql4.freemysqlhosting.net",
+
+    port: "3306",
+
+    user: "sql4460102",
+
+    password: "1THzJcyqZD",
+
+    database: "sql4460102"
+
+}
+
+
+
+module.exports = config;
